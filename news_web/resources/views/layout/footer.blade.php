3428822190acc377402e4923b1c7d59ff843b284
nguyenhoangviet-60131249 <62867246+nguyenhoangviet-60131249@users.noreply.github.com>
2021-10-26 14:14:38 +0700

    viet ok

--- news_web/resources/views/layout/footer.blade.php
+++
@@ -1,7 +0,0 @@
- <footer>
-        <div class="row">
-            <div class="col-md-12">
-                <p align='center'>Copyright &copy; Your Website 2021</p>
-            </div>
-        </div>
-    </footer>
