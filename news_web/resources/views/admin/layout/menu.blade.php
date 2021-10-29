c9f01ca5e5a7cac4024856c7ed2e4ff594646479
HoNguyen255 <91527028+HoNguyen255@users.noreply.github.com>
2021-10-29 21:40:38 +0700

    Update menu.blade.php

--- news_web/resources/views/admin/layout/menu.blade.php
+++ news_web/resources/views/admin/layout/menu.blade.php
@@ -6,7 +6,7 @@
                             <div class="input-group custom-search-form">
                                 <input type="text" class="form-control" placeholder="Search...">
                                 <span class="input-group-btn">
-                                    <button class="btn btn-default" type="button">
+                                    <button class="btn btn-success" type="button">
                                         <i class="fa fa-search"></i>
                                     </button>
                                 </span>
